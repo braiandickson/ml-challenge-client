0158d7abbe85e4c5c8b07ba6c603c9170fcf7a93
Braian Dickson <braiandickson@gmail.com>
2020-06-14 17:16:58 -0300

    Agrega libs, configuracion, variables de sass

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import cn from 'classnames';
 
 import styles from './styles.module.scss';
 
 function App() {
   return (
-    <div className={styles.container}>
+    <div className={cn('column center', styles.container)}>
       <span className={styles.title}>Hola</span>
     </div>
   );
