42f5f5c136e18c828c20703d882ceaaf80ec2f94
Braian Dickson <braiandickson@gmail.com>
2020-06-14 16:54:06 -0300

    Setup inicial junto con node-sass.

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>Hola</span>
+    </div>
+  );
+}
+
+export default App;
